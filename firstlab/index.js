ace804b67956d81970b99890757920e1c6e1519d
NikitaPeshko <n.peshko@mail.ru>
2022-03-10 10:45:14 +0300

    lab1

---
+++ firstlab/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import path from 'path'
+
+const __dirname=path.resolve()
+const PORT=3000
+const app=express()
+
+
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'static',"index.html"))
+})
+
+app.listen(PORT,()=>{
+    console.log("Server started on port 3000 ...")
+})
